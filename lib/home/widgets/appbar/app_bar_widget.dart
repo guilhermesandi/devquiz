d0ca35f4c2c9d756eb3783553d86bffddf7eb583
Guilherme Sandi <guilherme_sandi@hotmail.com>
2021-06-08 20:35:32 -0300

    Create AppBar

---
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -0,0 +1,38 @@
+import 'package:DevQuiz/core/core.dart';
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget()
+      : super(
+            preferredSize: Size.fromHeight(250),
+            child: Container(
+              height: 250,
+              decoration: BoxDecoration(gradient: AppGradients.linear),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text.rich(TextSpan(
+                        text: "Olá, ",
+                        style: AppTextStyles.title,
+                        children: [
+                          TextSpan(
+                            text: "Guilherme",
+                            style: AppTextStyles.titleBold,
+                          )
+                        ])),
+                    Container(
+                      width: 58,
+                      height: 58,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://avatars.githubusercontent.com/u/38008649?v=4"))),
+                    )
+                  ],
+                ),
+              ),
+            ));
+}
